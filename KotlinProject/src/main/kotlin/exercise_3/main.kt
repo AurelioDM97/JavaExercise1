196a0362131cc533f86ed4afbd7116727b4adbfd
AurelioDM97 <126873779+AurelioDM97@users.noreply.github.com>
2023-04-09 16:12:12 +0200

    Delete KotlinProject directory

--- KotlinProject/src/main/kotlin/exercise_3/main.kt
+++
@@ -1,7 +0,0 @@
-package exercise_3
-
-fun main() {
-    message(User.Email("None", "aureliodimarco@gmail.com"))
-    message(User.Facebook("Aurelio Di Marco", "aurelio@hotmail.it"))
-    message(User.Google("aureliodimarco97", "aureliodimarco97@gmail.com"))
-}
